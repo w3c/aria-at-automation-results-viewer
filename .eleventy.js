1fd643632176a1782ee0e6092f6ed256d6c2e715
Simon Pieters <zcorpan@gmail.com>
2021-12-17 01:53:25 +0100

    Show detailed results for each test

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,50 @@
 module.exports = function(eleventyConfig) {
   let tableify = require("tableify");
 
-  eleventyConfig.addShortcode("resultsTable", function(value) {
+  eleventyConfig.addShortcode("resultsTable", function(value, testPageName) {
     const table = [];
+
+    function addRow(testName) {
+      const existingRow = table.find(x => x.Test === testName);
+      const row = existingRow || { Test: testName };
+      if (!existingRow) {
+        table.push(row);
+      }
+      return row;
+    }
     for (const item in value) {
       for (const test in value[item]) {
-      	const testName = test.substr('aria-at-harness-result-'.length);
-      	const existingRow = table.find(x => x.Test === testName);
-        let row = existingRow || {};
-        if (!existingRow) {
-        	table.push(row);
+      	const testName = test;
+        if (!testPageName) {
+          // index page
+          const row = addRow(testName);
+          let testsPassed = 0;
+          for (const subTest of value[item][test].tests) {
+          	const results = subTest.results[0].results;
+          	if (results.filter(result => result.pass === true).length === results.length) {
+          		testsPassed++;
+          	}
+          }
+          const testsTotal = value[item][test].tests.length;
+          row[item] = `${testsPassed} / ${testsTotal}`;
+        } else {
+          // results for one test
+          if (testName !== testPageName) {
+            continue;
+          }
+          for (const subTest of value[item][test].tests) {
+            const testName = subTest.filepath.replace(".collected.json", "");
+            const row = addRow(testName);
+            row[item] = subTest.log;
+          }
         }
-        row.Test = testName;
-        let testsPassed = 0;
-        for (const subTest of value[item][test].tests) {
-        	const results = subTest.results[0].results;
-        	if (results.filter(result => result.pass === true).length === results.length) {
-        		testsPassed++;
-        	}
-        }
-        const testsTotal = value[item][test].tests.length;
-        row[item] = `${testsPassed} / ${testsTotal}`;
       }
     }
-    return tableify(table);
+    let rv = tableify(table);
+    if (!testPageName) {
+      rv = rv.replace(/<tr><td class="string">([^<]+)/g, '<tr><td class="string"><a href="/results/$1">$1</a>');
+    }
+    return rv;
   });
 
   eleventyConfig.setDataDeepMerge(true);
@@ -31,7 +52,7 @@ module.exports = function(eleventyConfig) {
   return {
     passthroughFileCopy: true,
   	dir: {
-  		input: "results",
+  		input: "pages",
   		output: "_site",
   		data: "../_data",
   		includes: "../_includes"
--- pages/index.md
+++ pages/index.md
@@ -4,8 +4,6 @@ title: aria-at.report
 ---
 # aria-at.report
 
-<!-- TODO iterate over all results, have links and nicer tables -->
-
 ## Test results
 
 {% resultsTable results %}
