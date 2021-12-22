6502696be19ef29d00425233b812748900a672f4
Simon Pieters <zcorpan@gmail.com>
2021-12-22 22:59:57 +0100

    Include rows for skipped tests

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   let tableify = require("tableify");
   const pathPrefix = 'aria-at-automation-results-viewer'; // This is for deployment to GitHub pages
 
-  eleventyConfig.addShortcode("resultsTable", function(value, testPageName) {
+  eleventyConfig.addShortcode("resultsTable", function(value, tests, testPageName) {
     const table = [];
 
     function addRow(testName) {
@@ -14,26 +14,30 @@ module.exports = function(eleventyConfig) {
       return row;
     }
     for (const item in value) {
-      for (const test in value[item]) {
-      	const testName = test;
+      for (const test of tests) {
+        const testName = test.name;
         if (!testPageName) {
           // index page
           const row = addRow(testName);
-          let testsPassed = 0;
-          for (const subTest of value[item][test].tests) {
-          	const results = subTest.results[0].results;
-          	if (results.filter(result => result.pass === true).length === results.length) {
-          		testsPassed++;
-          	}
+          if (!(testName in value[item])) {
+            row[item] = "No results.";
+          } else {
+            let testsPassed = 0;
+            for (const subTest of value[item][testName].tests) {
+              const results = subTest.results[0].results;
+              if (results.filter(result => result.pass === true).length === results.length) {
+                testsPassed++;
+              }
+            }
+            const testsTotal = value[item][testName].tests.length;
+            row[item] = `${testsPassed} / ${testsTotal}`;
           }
-          const testsTotal = value[item][test].tests.length;
-          row[item] = `${testsPassed} / ${testsTotal}`;
         } else {
           // results for one test
-          if (testName !== testPageName) {
+          if (!(testName in value[item]) || testName !== testPageName) {
             continue;
           }
-          for (const subTest of value[item][test].tests) {
+          for (const subTest of value[item][testName].tests) {
             const testName = subTest.filepath.replace(".collected.json", "");
             const row = addRow(testName);
             row[item] = subTest.log.map(item => item.text).join('\n');
@@ -41,11 +45,14 @@ module.exports = function(eleventyConfig) {
         }
       }
     }
+    if (table.length === 0) {
+      return "<p>There are no results for this test.</p>";
+    }
     let rv = tableify(table);
     if (!testPageName) {
       rv = rv.replace(/<tr><td class="string">([^<]+)/g, `<tr><td class="string"><a href="${ pathPrefix }/results/$1">$1</a>`);
     } else {
-    	rv = rv.replace(/<\/td><td class="string">([^<]+)/g, '</td><td class="string"><pre>$1</pre>');
+      rv = rv.replace(/<\/td><td class="string">([^<]+)/g, '</td><td class="string"><pre>$1</pre>');
     }
     return rv;
   });
@@ -55,11 +62,11 @@ module.exports = function(eleventyConfig) {
   return {
     passthroughFileCopy: true,
     pathPrefix,
-  	dir: {
-  		input: "pages",
-  		output: "_site",
-  		data: "../_data",
-  		includes: "../_includes"
-  	}
+    dir: {
+      input: "pages",
+      output: "_site",
+      data: "../_data",
+      includes: "../_includes"
+    }
   };
 }
